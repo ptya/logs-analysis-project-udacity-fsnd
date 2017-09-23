42fe7c1fd13fc85450cb4a83783c72483b023a6a
ptya <petr.szab@gmail.com>
2017-09-23 11:34:56 +0200

    Refactored execution with signle query function

--- reporting.py
+++ reporting.py
@@ -4,123 +4,144 @@
 import psycopg2
 import datetime
 import time
-import sys
 
 START_TIME = time.time()
 DBNAME = "news"
 
 def connect_db(db_name=DBNAME):
-    """Return connection to database and cursor."""
+    """Return connection to database and cursor.
+
+        args:
+            db_name - name of the database, default DBNAME
+
+        returns:
+            Database connection
+            Cursor to the database
+    """
     try:
         conn = psycopg2.connect(database=DBNAME)
         c = conn.cursor()
         return conn, c
-except (Exception, psycopg2.DatabaseError) as error:
+    except (Exception, psycopg2.DatabaseError) as error:
         print(error)
-        sys.exit()
 
 
-def top_3_articles():
-    """Return the top 3 articles, most viewed first."""
-    conn, c = connect_db()
-    query = """
-            SELECT title, views
-            FROM articles
-            INNER JOIN
-                (SELECT path, count(path)::integer AS views
-                 FROM log
-                 GROUP BY path) AS log
-            ON path = '/article/' || slug
-            ORDER BY views DESC
-            LIMIT 3
-            """
-    c.execute(query)
-    result = c.fetchall()
-    conn.close()
-    return result
+def execute_query(query):
+    """Takes an SQL query and returns as a list of tuples.
+
+        args:
+            query - an SQL query statement to be executed.
+
+        returns:
+            A list of tuples containing the results of the query.
+    """
+    try:
+        conn, c = connect_db()
+        c.execute(query)
+        result = c.fetchall()
+        conn.close()
+        return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
-def popular_authors():
-    """Return the most popular authors, most viewed first."""
-    conn, c = connect_db()
-    query = """
-        SELECT authors.name, sum(views) AS total_views
-        FROM articles
-        INNER JOIN
-            (SELECT path, count(path)::integer AS views
-             FROM log
-             GROUP BY path) AS log
-        ON path = '/article/' || slug
-        INNER JOIN authors
-        ON authors.id = articles.author
-        GROUP BY authors.name
-        ORDER BY total_views DESC
-        """
-    c.execute(query)
-    result = c.fetchall()
-    conn.close()
-    return result
+if __name__ == '__main__':
+    # Welcome message
+    print('\n## Fetching data. Please wait.. ##')
 
+    # 1 - Print top 3 articles, most viewed first.
+    # Output example:
+    # "Princess Shellfish Marries Prince Handsome" - 1201 views
+    # SQL query
+    query1 = """
+    SELECT title, views
+    FROM articles
+    INNER JOIN
+    (SELECT path, count(path)::integer AS views
+    FROM log
+    GROUP BY path) AS log
+    ON path = '/article/' || slug
+    ORDER BY views DESC
+    LIMIT 3
+    """
+    
+    # Fetch rows
+    msg1 = execute_query(query1)
 
-def error_days():
-    """Return the days where error request count was above 1%."""
-    conn, c = connect_db()
-    query = """
-        SELECT day, errors/total AS percentage
-        FROM (SELECT time::date AS day,
-                     COUNT(status) AS total,
-                     SUM((status NOT LIKE '%200%')::int)::float AS errors
-              FROM log
-              GROUP BY day) AS pretable
-        WHERE errors/total > 0.01
-        """
-    c.execute(query)
-    result = c.fetchall()
-    conn.close()
-    return result
+    # Print header
+    hdr1 = """
+    #############################
+    #  The top 3 articles are:  #
+    #############################
+    """
+    print(hdr1)
 
+    # Print results
+    for article, views in msg1:
+        print('"' + article + '" — ' + '{:,}'.format(views) + ' views')
 
-# Welcome message
-print('\n## Fetching data. Please wait.. ##')
+    # 2 - Most popular authors, most viewed first.
+    # Output example:
+    # Ursula La Multa — 2304 views
+    # SQL query
+    query2 = """
+    SELECT authors.name, sum(views) AS total_views
+    FROM articles
+    INNER JOIN
+    (SELECT path, count(path)::integer AS views
+    FROM log
+    GROUP BY path) AS log
+    ON path = '/article/' || slug
+    INNER JOIN authors
+    ON authors.id = articles.author
+    GROUP BY authors.name
+    ORDER BY total_views DESC
+    """
 
-# 1 - Print top 3 articles
-# Output example:
-# "Princess Shellfish Marries Prince Handsome" - 1201 views
-hdr1 = """
-#############################
-#  The top 3 articles are:  #
-#############################
-"""
-msg1 = top_3_articles()
-print(hdr1)
-for article, views in msg1:
-    print('"' + article + '" — ' + '{:,}'.format(views) + ' views')
+    # Fetch rows
+    msg2 = execute_query(query2)
 
-# 2 - Most popular authors
-# Output example:
-# Ursula La Multa — 2304 views
-hdr2 = """
-##################################
-#  The most popular actors are:  #
-##################################
-"""
-msg2 = popular_authors()
-print(hdr2)
-for author, views in msg2:
-    print(author + ' — ' + '{:,}'.format(views) + ' views')
+    # Print header
+    hdr2 = """
+    ##################################
+    #  The most popular actors are:  #
+    ##################################
+    """
+    print(hdr2)
 
-# 3 - Days above 1% request errors
-# Output example:
-# July 29, 2016 — 2.5% errors
-hdr3 = """
-###################################
-#  Days above 1% request errors:  #
-###################################
-"""
-msg3 = error_days()
-print(hdr3)
-for day, percent in msg3:
-    print(day.strftime("%B %d, %Y") + ' — ' + '{:.2%}'.format(percent))
+    # Print results
+    for author, views in msg2:
+        print(author + ' — ' + '{:,}'.format(views) + ' views')
 
-print('\n## End of program — runtime: %s seconds ##\n' %
-      round((time.time() - START_TIME), 2))
+    # 3 - Days above 1% request errors
+    # Output example:
+    # July 29, 2016 — 2.5% errors
+    # SQL query
+    query3 = """
+    SELECT day, errors/total AS percentage
+    FROM (SELECT time::date AS day,
+    COUNT(status) AS total,
+    SUM((status NOT LIKE '%200%')::int)::float AS errors
+    FROM log
+    GROUP BY day) AS pretable
+    WHERE errors/total > 0.01
+    """
+
+    # Fetch rows
+    msg3 = execute_query(query3)
+
+    # Print header
+    hdr3 = """
+    ###################################
+    #  Days above 1% request errors:  #
+    ###################################
+    """
+    print(hdr3)
+
+    # Print results
+    for day, percent in msg3:
+        print(day.strftime("%B %d, %Y") + ' — ' + '{:.2%}'.format(percent))
+
+    # Goodby message
+    print('\n## End of program — runtime: %s seconds ##\n' %
+          round((time.time() - START_TIME), 2))
